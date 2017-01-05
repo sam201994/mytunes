2414f0ea317fd0247cf175ef51f3727e38f48b40
Hack Reactor Students <accounts@hackreactor.com>
2017-01-04 21:04:18 -0800

    implement app, appview, and libraryview tests

--- test/spec/LibraryViewSpec.js
+++ test/spec/LibraryViewSpec.js
@@ -32,13 +32,14 @@ describe('LibraryView', function() {
     expect(view.$el.children()[0].tagName).to.equal('TH');
   });
   
-  xdescribe('when fetching song data from Parse', function() {
+  describe('when fetching song data from Parse', function() {
     var fakeResponse, requests, xhr;
 
     beforeEach(function() {
       requests = [];
       xhr = sinon.useFakeXMLHttpRequest();
       xhr.onCreate = function(request) {
+        console.log('helllllloooo')
         requests.push(request);
       };
 
@@ -53,7 +54,6 @@ describe('LibraryView', function() {
       var FakeLibraryView = LibraryView.extend({ render: sinon.spy() });
       view = new FakeLibraryView({ collection: new Songs() });
       expect(view.render).to.have.been.calledOnce;
-
       requests[0].respond(200, { 'Content-Type': 'application/json' }, fakeResponse);
       expect(view.render).to.have.been.calledTwice;
     });
